a41c7d3f6aed68cb6066fd461f29c95b01cc5cb9
TeeradejLo <teeradejlo.2002@gmail.com>
2022-04-26 16:37:44 +0800

    Edit Header State Transition

---
+++ src/Header.js
@@ -0,0 +1,90 @@
+import './Header.css';
+import Logo_red from './Airbnb_red.png';
+import Logo_white from './Airbnb_white.png';
+import { grey } from '@mui/material/colors';
+import LanguageIcon from '@mui/icons-material/Language';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import { useState, useEffect, useRef } from 'react';
+import { useWindowScrollPositions } from './useWindowScrollPosition';
+
+function Header() {
+    const [headerColor, setHeaaderColor] = useState("transparent");
+    const [logoColorWhite, setLogoColorWhite] = useState(1);
+    const [inputTabSize, setInputTabSize] = useState(1);
+    const [inputTabPos, setInputTabPos] = useState("static");
+    const [textColor, setTextColor] = useState("white");
+    const [theme, setTheme] = useState(["dark", "darkicon"]);
+    const [headerHeight, setHeaderHeight] = useState(["", ""]);
+    const headerCompHeight = useRef(["", ""]);
+
+    const {scrollY} = useWindowScrollPositions();
+
+    useEffect(() => {
+        if (scrollY > 200) {
+            setHeaaderColor("white");
+            setLogoColorWhite(0);
+            setInputTabSize(0);
+            setTextColor("black");
+            setTheme(["light", "lighticon"]);
+            setInputTabPos("absolute");
+            setHeaderHeight(headerCompHeight.current[1]);
+        } else {
+            setHeaaderColor("transparent");
+            setLogoColorWhite(1);
+            setInputTabSize(1);
+            setTextColor("white");
+            setTheme(["dark", "darkicon"]);
+            setInputTabPos("static");
+            setHeaderHeight(headerCompHeight.current[0]);
+        }
+    }, [scrollY]);
+
+    useEffect(() => {
+        let element1 = document.getElementById("home-header");
+        let computedStyle1 = getComputedStyle(element1);
+
+        let element2 = document.getElementById("home-header-input");
+        let computedStyle2 = getComputedStyle(element2);
+        headerCompHeight.current[0] = (element1.clientHeight - parseFloat(computedStyle1.paddingTop) - parseFloat(computedStyle1.paddingBottom)) + "px";
+        headerCompHeight.current[1] = (element1.clientHeight - parseFloat(computedStyle1.paddingTop) - parseFloat(computedStyle1.paddingBottom) - element2.clientHeight - parseFloat(computedStyle2.marginTop)) + "px";
+        console.log(headerCompHeight);
+
+        setHeaderHeight(headerCompHeight.current[0]);
+    }, []);
+
+    const styles = { 
+        transform: `scale(${inputTabSize})`,
+        position: inputTabPos,
+    };
+
+    return (
+        <div className = "header" id="home-header" style = {{backgroundColor: headerColor, height: headerHeight}}>
+                <div className="header_left">
+                    <img  alt="logo" src={logoColorWhite ? Logo_white : Logo_red}/>  
+                </div>
+                <div className="header_middle">
+                    <div className="header_mode" style={{color: textColor, transform: `scale(${inputTabSize})`, position: inputTabPos}}>
+                        <div variant={theme[0]}>Places to stay</div>
+                        <div variant={theme[0]}>Experiences</div>
+                        <div variant={theme[0]}>Online Experiences</div>
+                    </div>
+                    <div className="header_inputinfo" id="home-header-input" style={styles}>
+                        <div style = {{color: "black"}}>INPUT</div>
+                    </div>
+                </div>
+                <div className="header_right">
+                    <button variant={theme[0]} style={{ color: textColor, fontSize: 14}}>Become a Host</button>
+                    <button variant={theme[1]} style={{padding: "12px 12px"}}>
+                        <LanguageIcon sx={{ fontSize: 20, color: textColor }} style={{display: "block"}}/>
+                    </button>
+                    <div className="header_user_menu">
+                        <DensityMediumIcon sx={{ fontSize: 16 }}/>
+                        <AccountCircleIcon sx={{ color: grey[700], fontSize: 33, margin: "-7px -7px -7px 6px"}}/>
+                    </div>
+                </div>
+            </div>
+    );
+}
+
+export default Header;
